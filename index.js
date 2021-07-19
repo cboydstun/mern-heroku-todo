012fa57c7b2d803dc4f8e5d1edbd2cbbb5e07435
Chris Boydstun <52934303+cboydstun@users.noreply.github.com>
2021-07-18 19:29:04 -0500

    revised filed structure

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 //import dependencies
-const express = require('express')
-const mongoose = require('mongoose')
-const cors = require('cors')
-const morgan = require('morgan')
-require('dotenv').config();
+import express from 'express';
+import mongoose from 'mongoose';
+import cors from 'cors';
+import morgan from 'morgan'
+import path from 'path'
+import dotenv from 'dotenv'
+
+dotenv.config();
 
 //initalize express
 const app = express()
@@ -12,7 +15,7 @@ const app = express()
 const SERVER_PORT = process.env.PORT || 5002;
 
 //initalize middleware
-app.use(express.json());
+app.use(express.json({ extended: false }));
 app.use(cors());
 app.use(morgan(':method :url :response-time'))
 
@@ -24,13 +27,20 @@ mongoose.connect(process.env.MONGO_URL, {
 }).then(console.log("Connected to MongoDB")).catch((err)=>{console.log(err)})
 
 //import routes
-const taskRouter = require("./routes/tasks")
 
 //initalize routes
-app.use('/api/tasks', taskRouter)
 
 //basic greeting
 app.get('/', (req, res) => {res.send('Hello World!')})
+
+//Heroku watching for production build
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+  app.get("*", (req, res) => {
+    const __dirname = path.dirname(new URL(import.meta.url).pathname);
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 //app listening
 app.listen(SERVER_PORT, ()=>{console.log(`Server running at ${SERVER_PORT}`)})
