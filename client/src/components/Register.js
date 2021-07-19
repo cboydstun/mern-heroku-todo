4c7483a4340d0fdae7c79909ab690a3fcb0ab47f
Chris Boydstun <52934303+cboydstun@users.noreply.github.com>
2021-07-19 07:33:42 -0500

    cleanup

--- client/src/components/Register.js
+++ client/src/components/Register.js
@@ -72,9 +72,6 @@ const Register = () => {
             type="email"
             placeholder="Enter email"
           />
-          <Form.Text className="text-muted">
-            We'll never share your email with anyone else.
-          </Form.Text>
         </Form.Group>
 
         <Form.Group controlId="formBasicPassword">
